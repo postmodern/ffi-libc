779b467718eb272f6fa84a0c5d2c8639076fbbfb
postmodern <postmodern.mod3@gmail.com>
2010-05-20 03:17:43 -0700

    Removed redudent (and incorrect) typedefs.

--- lib/ffi/libc/types.rb
+++ lib/ffi/libc/types.rb
@@ -4,11 +4,6 @@ module FFI
   module LibC
     extend FFI::Library
 
-    typedef :uint, :size_t
-    typedef :int, :ssize_t
-    typedef :uint, :off_t
-    typedef :ulong, :time_t
-    typedef :long, :suseconds_t
     typedef :pointer, :FILE
   end
 end

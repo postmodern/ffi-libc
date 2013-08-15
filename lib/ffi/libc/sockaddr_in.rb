551874a94a2b4cfd52de9a388f41d3db1df3026a
Postmodern <postmodern.mod3@gmail.com>
2013-08-15 15:59:42 -0700

    Style.

--- lib/ffi/libc/sockaddr_in.rb
+++ lib/ffi/libc/sockaddr_in.rb
@@ -8,10 +8,10 @@ module FFI
     #
     class SockaddrIn < SockaddrFamily
 
-      layout :len, :uint8,
-             :family, :sa_family_t,
-             :port, :in_port_t,
-             :addr, :in_addr_t,
+      layout :len,      :uint8,
+             :family,   :sa_family_t,
+             :port,     :in_port_t,
+             :addr,     :in_addr_t,
              :_sa_zero, [:uint8, 8]
 
     end

719ca1dbc87745a266fdfbcd3f1a7a90d3562a56
Postmodern <postmodern.mod3@gmail.com>
2013-08-15 15:47:23 -0700

    Added specs for each_ifaddr.

--- spec/libc_spec.rb
+++ spec/libc_spec.rb
@@ -1,15 +1,36 @@
 require 'spec_helper'
+require 'ffi/libc'
 
-describe "FFI::LibC" do
-  it "should attach all functions" do
-    lambda {
-      require 'ffi/libc/libc'
-    }.should_not raise_error
+describe FFI::LibC do
+  describe "NULL" do
+    subject { described_class::NULL }
+
+    it { should == nil }
   end
 
-  it "should be able to fetch its own memory usage" do
-    require 'ffi/libc'
-    ru = FFI::LibC.getrusage
-    ru[:ru_maxrss].should be > 4000
+  describe "each_ifaddr" do
+    it "should yield Ifaddrs" do
+      addrs = []
+
+      described_class.each_ifaddr do |ifaddr|
+        addrs << ifaddr
+      end
+
+      addrs.all? { |addr| addr.kind_of?(Ifaddrs) }.should be_true
+    end
+
+    context "when not given a block" do
+      subject { described_class.each_ifaddr }
+
+      it { should be_kind_of(Enumerable) }
+    end
+  end
+
+  describe "rusage" do
+    subject { described_class.rusage }
+
+    it "should be able to fetch its own memory usage" do
+      subject[:ru_maxrss].should be > 4000
+    end
   end
 end

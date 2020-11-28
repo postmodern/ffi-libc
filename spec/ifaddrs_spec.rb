bedd2af058c5c4662aa2bfeadebe271ec71ca8f0
Postmodern <postmodern.mod3@gmail.com>
2020-11-27 22:40:21 -0800

    Updated rspec syntax to 3.x.

--- spec/ifaddrs_spec.rb
+++ spec/ifaddrs_spec.rb
@@ -5,26 +5,26 @@ describe Ifaddrs do
   subject { LibC.each_ifaddr.first }
 
   it "should have a name" do
-    subject.name.should_not be_empty
+    expect(subject.name).to_not be_empty
   end
 
   it "should have flags" do
-    subject.flags.should be_kind_of(Integer)
+    expect(subject.flags).to be_kind_of(Integer)
   end
 
   it "should have an addr" do
-    subject.addr.should be_kind_of(Sockaddr)
+    expect(subject.addr).to be_kind_of(Sockaddr)
   end
 
   it "should have an netmask" do
-    subject.netmask.should be_kind_of(Sockaddr)
+    expect(subject.netmask).to be_kind_of(Sockaddr)
   end
 
   it "should have a broadcast addr" do
-    subject.broadaddr.should be_kind_of(Sockaddr)
+    expect(subject.broadaddr).to be_kind_of(Sockaddr)
   end
 
   it "should have a destination addr" do
-    subject.dstaddr.should be_kind_of(Sockaddr)
+    expect(subject.dstaddr).to be_kind_of(Sockaddr)
   end
 end
--- spec/libc_spec.rb
+++ spec/libc_spec.rb
@@ -5,7 +5,7 @@ describe FFI::LibC do
   describe "NULL" do
     subject { described_class::NULL }
 
-    it { should == nil }
+    it { expect(subject).to be == nil }
   end
 
   describe "each_ifaddr" do
@@ -16,13 +16,13 @@ describe FFI::LibC do
         addrs << ifaddr
       end
 
-      addrs.all? { |addr| addr.kind_of?(Ifaddrs) }.should be_true
+      expect(addrs.all? { |addr| addr.kind_of?(Ifaddrs) }).to be_true
     end
 
     context "when not given a block" do
       subject { described_class.each_ifaddr }
 
-      it { should be_kind_of(Enumerable) }
+      it { expect(subject).to be_kind_of(Enumerable) }
     end
   end
 
@@ -30,7 +30,7 @@ describe FFI::LibC do
     subject { described_class.rusage }
 
     it "should be able to fetch its own memory usage" do
-      subject[:ru_maxrss].should be > 4000
+      expect(subject[:ru_maxrss]).to be > 4000
     end
   end
 end
